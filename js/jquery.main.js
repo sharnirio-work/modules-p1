053b6b908e1938a6d09f01f6021de88ef34e7107
sharnirio <sharnirio@gmail.com>
2021-12-20 17:55:10 +0200

    Create a few new module

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -146,3 +146,22 @@ function initMap(elementId) {
 		}
 	});
 }
+
+/* module AspectRatioBoxes */
+
+
+/* module buttonWithOverlay */
+// function on
+jQuery(function() {
+	isElementExist(".sbutton", buttonWithOverlayInit);
+});
+// function init
+function buttonWithOverlayInit() {
+	$(".sbutton").on("mousemove", function(t) {
+		var e = $(this),
+			i = e.offset(),
+			s = t.pageY - i.top,
+			n = t.pageX - i.left;
+		t.target.style.setProperty("--x", "".concat(n, "px")), t.target.style.setProperty("--y", "".concat(s, "px"));
+	});
+}
